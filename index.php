d60b881f409a2c988be1d4f18e5fadeceb586d2d
Abdullah Emin Şahin <abdullaheminsahin23@gmail.com>
2025-06-08 10:47:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -514,7 +514,7 @@
  
   <script src="js/custom.js"></script>
 
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
+  <script src=>
   </script>
   
 
